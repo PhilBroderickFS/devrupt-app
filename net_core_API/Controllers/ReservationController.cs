3812aec85ef627a57b1bdaf6c0599e863c15501b
Olatayo Joshua Olukotun <olukotunjosh@gmail.com>
2021-03-08 04:14:53 +0200

    Created Index and Detail action in Reservation Controller

---
+++ net_core_API/Controllers/ReservationController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using net_core_API.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace net_core_API.Controllers
+{
+    public class ReservationController : Controller
+    {
+
+        private readonly IHttpClientFactory _httpclientFactory;
+
+        public ReservationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpclientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Reservation> reservationList = new List<Reservation>();
+
+            var httpClient = _httpclientFactory.CreateClient("apaleo");
+            var URL = $"/booking/v1/reservations";
+
+            var response = await httpClient.GetAsync("URL");
+
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            reservationList = JsonConvert.DeserializeObject<List<Reservation>>(apiResponse);
+
+            return View(reservationList);
+
+        }
+
+
+        public async Task<IActionResult> Detail(string id)
+        {
+            Reservation reservation = new Reservation();
+
+            var httpClient = _httpclientFactory.CreateClient("apaleo");
+            var URL = $"/booking/v1/reservations/{id}";
+
+            var response = await httpClient.GetAsync("URL");
+
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            reservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+
+            return View(reservation);
+
+        }
+    }
+}
--- net_core_API/Startup.cs
+++ net_core_API/Startup.cs
@@ -1,21 +1,32 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace net_core_API
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+
+
+            services.AddHttpClient("apaleo", client =>
+            {
+                client.BaseAddress = new Uri(configuration.GetValue<string>("APALEO_BASE_URL"));
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +37,16 @@ namespace net_core_API
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseRouting();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Reservation}/{action=Index}/{id?}");
             });
         }
     }
