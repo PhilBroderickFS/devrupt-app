f96396d7435d33db09516b8edec732c6b3089585
PhilBroderick <philbroderick12@gmail.com>
2021-03-27 10:40:37 +0000

    retrieve set/dish and ingredient data from backend

--- ui/src/app/core/services/dish.service.ts
+++ ui/src/app/core/services/dish.service.ts
@@ -1,246 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Observable, of, BehaviorSubject } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Set } from '../../shared/models/set.model';
 import { Ingredient } from 'src/app/shared/models/ingredient.model';
 import { Dish } from 'src/app/shared/models/dish.model';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DishService {
 
+  private apiUrl = 'https://localhost:5001/api/Recommendation';
 
   sets: Set[] = [
-    {
-      name: "Set A",
-      compatability: 70,
-      dishes: [
-        {
-          name: "Dish A",
-          ingredients: [
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            }
-          ]
-        },
-        {
-          name: "Dish B",
-          ingredients: [
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            },
-            {
-              name: "Ingredient B",
-              amount: 200,
-              measurement: "ml"
-            }
-          ]
-        },
-        {
-          name: "Dish C",
-          ingredients: [
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            },
-            {
-              name: "Ingredient B",
-              amount: 200,
-              measurement: "ml"
-            }
-          ]
-        },
-        {
-          name: "Dish D",
-          ingredients: [
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            }
-          ]
-        },
-        {
-          name: "Dish E",
-          ingredients: [
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            },
-            {
-              name: "Ingredient B",
-              amount: 200,
-              measurement: "ml"
-            }
-          ]
-        }
-      ]
-    },
-    {
-      name: "Set B",
-      compatability: 60,
-      dishes: [
-        {
-          name: "Dish A",
-          ingredients: [
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            }
-          ]
-        },
-        {
-          name: "Dish B",
-          ingredients: [
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            },
-            {
-              name: "Ingredient B",
-              amount: 200,
-              measurement: "ml"
-            }
-          ]
-        },
-        {
-          name: "Dish C",
-          ingredients: [
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            },
-            {
-              name: "Ingredient B",
-              amount: 200,
-              measurement: "ml"
-            }
-          ]
-        },
-        {
-          name: "Dish D",
-          ingredients: [
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            }
-          ]
-        },
-        {
-          name: "Dish E",
-          ingredients: [
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            },
-            {
-              name: "Ingredient B",
-              amount: 200,
-              measurement: "ml"
-            }
-          ]
-        }
-      ]
-    },
-    {
-      name: "Set C",
-      compatability: 45,
-      dishes: [
-        {
-          name: "Dish A",
-          ingredients: [
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            },
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            }
-          ]
-        },
-        {
-          name: "Dish B",
-          ingredients: [
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            },
-            {
-              name: "Ingredient B",
-              amount: 200,
-              measurement: "ml"
-            }
-          ]
-        },
-        {
-          name: "Dish C",
-          ingredients: [
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            },
-            {
-              name: "Ingredient B",
-              amount: 200,
-              measurement: "ml"
-            }
-          ]
-        },
-        {
-          name: "Dish D",
-          ingredients: [
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            }
-          ]
-        },
-        {
-          name: "Dish E",
-          ingredients: [
-            {
-              name: "Ingredient A",
-              amount: 3,
-              measurement: "kg"
-            },
-            {
-              name: "Ingredient B",
-              amount: 200,
-              measurement: "ml"
-            }
-          ]
-        }
-      ]
-    }
+
   ]
 
-  public dishesPerSet: BehaviorSubject<number> = new BehaviorSubject<number>(3);
+  public dishesPerSet: BehaviorSubject<number> = new BehaviorSubject<number>(5);
   filteredSetList = this.filterDishesPerSets();
 
-  public set$: BehaviorSubject<Set[]> = new BehaviorSubject(this.filteredSetList);
-  public selectedSet: BehaviorSubject<Set> = new BehaviorSubject(this.filteredSetList[0]);
-  public selectedDishes: BehaviorSubject<Dish[]> = new BehaviorSubject<Dish[]>(this.selectedSet.value.dishes);
-  public selectedIngredientTotals: BehaviorSubject<Ingredient[]> = new BehaviorSubject<Ingredient[]>(this.getIngredientTotals())
+  public set$: BehaviorSubject<Set[] | null> = new BehaviorSubject(null);
+  public selectedSet: BehaviorSubject<Set | null> = new BehaviorSubject(null);
+  public selectedDishes: BehaviorSubject<Dish[]| null> = new BehaviorSubject(null);
+  public selectedIngredientTotals: BehaviorSubject<Ingredient[]| null> = new BehaviorSubject(null);
 
-  constructor() { }
-  
+  constructor(private httpClient: HttpClient) { }
+
   setSelectedSet(name: string) {
     let newSelectedSet = this.filteredSetList.find(set => set.name.toUpperCase() === name.toUpperCase());
     this.selectedSet.next(newSelectedSet);
@@ -248,8 +34,16 @@ export class DishService {
     this.selectedIngredientTotals.next(this.getIngredientTotals());
   }
 
-  getSets(date: Date): Observable<Set[]> {
-    return of(this.sets);
+  getSets(guestIds: string[]): Observable<Set[]> {
+    return this.httpClient.post<Set[]>(this.apiUrl, { 'numberOfDishes': this.dishesPerSet.value, 'guestIds': guestIds})
+      .pipe(
+        map(sets => {
+          this.sets = sets;
+          this.selectedSet.next(this.sets[0]);
+          this.updateObservables();
+          return sets;
+        })
+      );
   }
 
   setDishNumber(numOfDishes: number) {
